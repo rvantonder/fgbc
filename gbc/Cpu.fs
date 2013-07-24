991033d884d4bd20619f27ba9ae25156b820ca5b
rvantonder <rvantonder@gmail.com>
2013-07-24 18:57:39 +0200

    first commit

---
+++ gbc/Cpu.fs
@@ -0,0 +1,87 @@
+﻿open System
+
+type cpu = { A : byte; B : byte; C : byte; D : byte; E : byte; // registers
+             f_Z : byte; f_H : byte; f_N : byte; f_C : byte; // flags 
+             HL : uint16; SP : uint16; PC : uint16; CPU_Ticks : uint16}
+
+
+let state = { A = 0x00uy; B = 0x0uy; C = 0x00uy; D = 0x00uy; E = 0x00uy;
+                  f_Z = 0x00uy; f_N = 0x0uy; f_H = 0x00uy; f_C = 0x00uy;
+                  PC = 0x0000us; SP = 0x0000us; HL = 0x000dus; CPU_Ticks = 0x00us; }
+
+type reg = 
+    | A
+    | B
+    | C 
+    | D 
+    | E 
+    | F_Z
+    | F_H 
+    | F_N
+    | F_C
+    | HL 
+    | SP 
+    | PC 
+    | CPU_Ticks 
+    | Mλ of reg * reg
+
+let rec set v cpu = 
+    match v with
+    | A, x -> {cpu with A = x}
+    | B, x -> {cpu with B = x}
+    | C, x -> {cpu with C = x}
+    | D, x -> {cpu with D = x}
+    | E, x -> {cpu with E = x}
+    | F_Z, x -> {cpu with f_Z = x}
+    | F_H, x -> {cpu with f_H = x}
+    | F_N, x -> {cpu with f_N = x}
+    | F_C, x -> {cpu with f_C = x}
+    | HL, x -> {cpu with HL = (uint16) x}
+    | SP, x -> {cpu with SP = (uint16) x}
+    | PC, x -> {cpu with PC = (uint16) x}
+    | CPU_Ticks, x -> {cpu with CPU_Ticks = (uint16) x}
+    | Mλ(r1, r2), x -> set (r1, x) cpu |> set (r2, x)
+
+let rec modify v cpu = 
+    match v with
+    | A, x -> {cpu with A = cpu.A + x}
+    | B, x -> {cpu with B = cpu.B + x}
+    | C, x -> {cpu with C = cpu.C + x}
+    | D, x -> {cpu with D = cpu.D + x}
+    | E, x -> {cpu with E = cpu.E + x}
+    | PC, x -> {cpu with PC =  cpu.PC + (uint16) x}
+    | CPU_Ticks, x -> {cpu with CPU_Ticks = cpu.CPU_Ticks + (uint16) x}
+    | _ -> failwith "Can't do that for you sorry"
+
+let inc reg cpu = cpu |> modify (reg, 1uy) // actually accepts tuple
+let dec reg cpu = cpu |> modify (reg, (255uy)) // same as (0uy-1uy)
+//state |> set (A, 5uy) |> inc A |> inc B
+
+let read adr = 0x0uy // placeholder
+let write adr v = printfn "%d" adr //
+
+let inline (@) a b = BitConverter.ToUInt16([|a; b|], 0)
+let getBytes (n:uint16) = BitConverter.GetBytes(n)
+
+let exec op state = 
+    match op with
+    | 0x00 -> state // NOP
+    | 0x01 -> state |> inc PC |> set (C, read PC) |> inc PC |> set (B, read PC) |> modify (CPU_Ticks, 12uy)
+    | 0x02 -> write (state.B@state.C) state.A ; state |> modify (CPU_Ticks, 8uy)
+    | 0x03 -> state |> set (B, (getBytes ((state.B@state.C)+1us)).[0]) |> set (C, (getBytes ((state.B@state.C)+1us)).[1])
+    | 0x04 -> inc B state
+    | 0x05 -> dec B state
+    | 0x06 -> state |> inc PC |> set (B, read PC) |> modify (CPU_Ticks, 8uy)
+    | _ -> failwith "Sorry Dave, can't do that for you"
+
+let init_state = { A = 0x01uy; B = 0x00uy; C = 0x13uy; D = 0x00uy; E = 0xd8uy;
+                  f_Z = 0x01uy; f_N = 0x01uy; f_H = 0x01uy; f_C = 0x01uy;
+                  PC = 0x0100us; SP = 0xfffeus; HL = 0x014dus; CPU_Ticks = 0x00us; }
+
+let next_state = init_state |> exec 0x0
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
+
---
+++ gbc/TestZone.fsx
@@ -0,0 +1,110 @@
+﻿type cpu = { A : byte; B : byte; C : byte; D : byte; E : byte; // registers
+             f_Z : byte; f_H : byte; f_N : byte; f_C : byte; // flags 
+             HL : uint16; SP : uint16; PC : uint16; CPU_Ticks : uint16}
+
+
+let state = { A = 0x01uy; B = 0x0uy; C = 0x00uy; D = 0x00uy; E = 0x00uy;
+                  f_Z = 0x00uy; f_N = 0x0uy; f_H = 0x00uy; f_C = 0x00uy;
+                  PC = 0x0000us; SP = 0x0000us; HL = 0x000dus; CPU_Ticks = 0x00us; }
+type op =
+    | INC 
+    | DEC
+    | V of byte
+
+type reg = 
+    | A of op
+    | B of op
+    | C of op
+    | D of byte
+    | E of byte
+    | F_Z of byte
+    | F_H of byte
+    | F_N of byte
+    | F_C of byte
+    | HL of uint16
+    | SP of uint16
+    | PC of uint16
+    | CPU_Ticks of uint16
+    | Mλ of reg * reg
+
+type rg = 
+    | A
+    | B
+    | C 
+    | D 
+    | E 
+    | F_Z
+    | F_H 
+    | F_N
+    | F_C
+    | HL 
+    | SP 
+    | PC 
+    | CPU_Ticks 
+    | Mλ of rg * rg
+
+let rec modify cpu r = 
+    match r with
+    | A, x -> {cpu with A = cpu.A + x}
+    | A, y -> {cpu with A = y}
+    | PC, x -> {cpu with PC = cpu.PC + (uint16) x}
+    | Mλ(r1, r2), x -> modify cpu (r1, x) |> modify <| (r2, x)
+
+let inc_1 cpu r = inc 1uy cpu r
+    
+type t2 = {a : reg; b : reg}
+
+let rec update_cpu cpu = function // implicitly matches A..B.. etc
+    | A INC -> {cpu with A = cpu.A + 1uy}
+    | A (V v) -> {cpu with A = v}
+    //| x INC -> {cpu with x = cpu.x + 1uy }
+    | B v -> {cpu with B = v}
+    | C v -> {cpu with C = v}
+    | D v -> {cpu with D = v}
+    | E v -> {cpu with E = v}
+    | F_Z v -> {cpu with f_Z = v}
+    | F_H v -> {cpu with f_H = v}
+    | F_N v -> {cpu with f_N = v}
+    | F_C v -> {cpu with f_C = v}
+    | HL v -> {cpu with HL = v}
+    | SP v -> {cpu with SP = v}
+    | PC v -> {cpu with PC = v}
+    | CPU_Ticks v -> {cpu with CPU_Ticks = v}
+    | Mλ(v1, v2) -> update_cpu cpu v1 |> update_cpu <| v2
+
+let update_t2 t2 = function
+    | A v -> printfn "A"
+    | B v -> printfn "B"
+    | _ -> printfn "_"
+
+let c_add cpu r =
+    match r with
+    | A _ -> update_cpu cpu (A (cpu.A + 0x01uy))
+    | _ -> failwith "fail"
+
+
+
+//let q = { a = A(0x1uy) ; b = B(0x2uy)}
+//let updated = update_t2 q (B(0x3uy))
+//let updated2 = update_t2 q (B(q.a))
+
+
+let incc state = c_add state (A(0x0uy))
+incc state
+
+let set1 cpu r = update_cpu cpu <| r(1uy) // set to 1, r refers to a reg type
+let inc cpu r v = update_cpu cpu <| r(v+1uy) // r refers to reg type, not cpu reg, ambiguous
+let t = B(5uy) // if this is a reg... then we can match the state
+
+let state2 = inc state B state.B // don't like this, want: inc state B
+
+let inc_PC cpu = update_cpu cpu <| PC(cpu.PC + 1us)
+
+let init_cpu cpu = { A = 0x01uy; B = 0x0uy; C = 0x13uy; D = 0x0uy; E = 0xd8uy;
+                  f_Z = 0x01uy; f_N = 0x0uy; f_H = 0x01uy; f_C = 0x01uy;
+                  PC = 0x0100us; SP = 0xfffeus; HL = 0x014dus; CPU_Ticks = 0x0us; }
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+    0 // return an integer exit code
